e60c0421c8d01b9e8c3a9c483cd8c6d8daf20716
Brian Kosiadi <briankosiadi@gmail.com>
2026-03-05 10:48:21 -0800

    Lesson 12

--- src/Home.js
+++ src/Home.js
@@ -8,9 +8,13 @@ const Home = () => {
     { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const handleDelete = (id) => {
+        const newBlogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newBlogs);
+    }
     return ( 
         <div className="home">
-            <Bloglist blogs={blogs} title="All Blogs!"/>
+            <Bloglist blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
         </div>
      );
 }
