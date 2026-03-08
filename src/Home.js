8c0be03bbc276058fac6489769dacc2d1b9f1eb2
Brian Kosiadi <briankosiadi@gmail.com>
2026-03-08 15:48:13 -0700

    Lesson 18

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,24 @@ import Bloglist from './Bloglist';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
 
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-        .then(res => {
-            return res.json()
-        })
-        .then(data => {
-            setBlogs(data)
-        })
+        setTimeout(() => {
+             fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                setBlogs(data);
+                setIsPending(false);
+        });
+        }, 1000)
     }, []);
 
     return ( 
         <div className="home">
+            { isPending && <div>Loading...</div>}
             {blogs && <Bloglist blogs={blogs} title="All Blogs!"/>}
         </div>
      );
